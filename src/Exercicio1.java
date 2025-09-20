4974aa92d57630501e9e681eabc772ba884ec39d
williambritodev12 <williambritodev@gmail.com>
2025-09-20 10:02:55 -0300

    Inserindo estrutura para exercicio

---
+++ src/Exercicio1.java
@@ -0,0 +1,21 @@
+// Exercício 1: Explorando Variáveis no Sistema de Reservas de um Hotel 
+// Autor: [William Brito]
+// Data: [20-09-2025]
+
+
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        // 1. Declaração das variveis conforme o enunciado
+    int numeroQuarto = 101;
+    double valorDiaria = 350.50;
+    String nomeHospede = "William Carneiro Brito";
+    
+    // 2. Exibição dos valores formatados na tela
+    System.out.println("--- SISTEMA DE RESERVAS DO HOTEL ---");
+    System.out.println("-------------------------------------");
+    System.out.println("Nome do Hóspede: " + nomeHospede);
+    System.out.println("Número do Quarto: " + numeroQuarto);
+    System.out.println("Valor da Diária: R$ " + String.format("%.2f", valorDiaria));
+    }
+}
