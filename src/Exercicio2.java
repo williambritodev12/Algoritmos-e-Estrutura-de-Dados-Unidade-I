3f07082f2c761036e984ff58593b9ee4f647b1b4
williambritodev12 <williambritodev@gmail.com>
2025-09-20 11:52:35 -0300

    Exercício 2

---
+++ src/Exercicio2.java
@@ -0,0 +1,29 @@
+// Exercício 1: Verificador de Características de Um Produto
+// Autor: [William Brito]
+// Data: [20-09-2025]
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in); 
+
+    // Solicita ao usuário o código do produto
+    System.out.print("Digite o código do produto para verificação: ");
+    int codigoProduto = scanner.nextInt();
+
+
+    //  Verifica se o número é par ou ímpar usando o operador de módulo (%)
+    //  Se o resto da divisão por 2 for 0, o número é par.
+    if (codigoProduto % 2 == 0) {
+        System.out.println("O código " + codigoProduto + " é PAR.");
+    }
+    else {
+        System.out.println("O código " + codigoProduto + " é ÍMPAR.");
+    }
+
+    scanner.close();
+    }
+
+}
